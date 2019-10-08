b26272f01ff3511a45683123647a79f1f2624af6
Xornet <einnotwendigesubel@gmail.com>
2019-10-08 22:03:27 +0900

    remove unused function

--- template.cpp
+++ template.cpp
@@ -15,23 +15,6 @@ typedef long long ll;
 #define rep(i, n) for(int i = 0; i < n; i++)
 #define repr(i, a, b) for(int i = a; i < b; i++)
 
-long gcd(long a, long b) {
-    if (a < b) {
-        swap(a, b);
-    }
-
-    while (b != 0) {
-        a = a % b;
-        swap(a, b);
-    }
-
-    return a;
-}
-
-long lcm(long a, long b) {
-    return a * b / gcd(a, b);
-}
-
 int main() {
     return 0;
 }
