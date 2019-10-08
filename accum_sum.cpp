691551db0a8c135e8420cea7851642254f3a3f89
Xornet <einnotwendigesubel@gmail.com>
2019-10-08 22:03:41 +0900

    累積和

---
+++ accum_sum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+typedef long long ll;
+
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define repr(i, a, b) for(int i = a; i < b; i++)
+
+template<typename T>
+
+vector<T> accum_sum(vector<T> a) {
+    T l = a.size();
+    vector<T> ret(l);
+
+    ret[0] = a[0];
+
+    repr(i, 1, l) {
+        ret[i] = a[i] + ret[i - 1];
+    }
+
+    return ret;
+}
