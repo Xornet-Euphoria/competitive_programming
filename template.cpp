47080823c2f1bab9bf0bfd51232cfa0bc094337a
Xornet <einnotwendigesubel@gmail.com>
2019-10-08 23:50:12 +0900

    add macro: vec_all

--- template.cpp
+++ template.cpp
@@ -14,6 +14,7 @@ typedef long long ll;
 
 #define rep(i, n) for(int i = 0; i < n; i++)
 #define repr(i, a, b) for(int i = a; i < b; i++)
+#define vec_all(vec) vec.begin(), vec.end()
 
 int main() {
     return 0;
