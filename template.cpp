6d6cd1a9f787ee5de3db20d0e253947c2ac174b1
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2019-10-15 02:53:33 +0900

    trivial change

--- template.cpp
+++ template.cpp
@@ -21,7 +21,7 @@ typedef pair<int, int> p_i;
 #define reprev(i, n) for (int i = n - 1; i >= 0; i--)
 #define repextrev(i, a, b) for (int i = b - 1; i >= a; i--)
 
-#define repv(values) for (auto v : values)
+#define repv(v, values) for (auto v : values)
 
 #define vec_all(vec) vec.begin(), vec.end()
 #define sum(vec) accumulate(vec_all(vec), 0)
