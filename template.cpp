4bb931d1fa5cb39b6ca0d039ed7f866ec0aded56
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2019-10-16 16:28:15 +0900

    new macro: rev, revsrt

--- template.cpp
+++ template.cpp
@@ -26,6 +26,8 @@ typedef pair<int, int> p_i;
 #define vec_all(vec) vec.begin(), vec.end()
 #define sum(vec) accumulate(vec_all(vec), 0)
 #define srt(vec) sort(vec_all(vec))
+#define rev(vec) reverse(vec_all(vec))
+#define revsrt(vec) srt(vec); rev(vec)
 
 // vectorデバッグ
 template<typename T>
